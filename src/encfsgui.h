a4ee89d7d6cb828ade6ada7939903a533b59f4a6
Peter Van Eeckhoutte <peter.ve@corelan.be>
2016-01-23 23:23:24 +0100

    that's a better place

--- src/encfsgui.h
+++ src/encfsgui.h
@@ -6,6 +6,16 @@
 #endif
 
 #include <wx/config.h>
+#ifndef RLOG_COMPONENT
+//#  warning RLOG_COMPONENT not defined - setting to UNKNOWN
+#define RLOG_COMPONENT "[unknown]"
+#endif // RLOG_COMPONENT not defined
+#define FUSE_USE_VERSION 26
+
+#include "fuse/fuse.h"
+#include "openssl.h"
+#include "encfs.h"
+
 
 // ----------------------------------------------------------------------------
 // function declarations
